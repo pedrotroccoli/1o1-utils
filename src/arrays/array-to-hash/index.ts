45a70d25be7395d145ed61667ff3b305b4236098
troccoli <pedroaugustobelzkai@gmail.com>
2025-08-21 00:55:13 -0300

    fix: update arrayToHash function to correctly create a hash map from the input array

--- src/arrays/array-to-hash/index.ts
+++ src/arrays/array-to-hash/index.ts
@@ -1,7 +1,15 @@
 import { ArrayToHashParams, ArrayToHashResult } from "./types";
 
 function arrayToHash<T>({ array, key }: ArrayToHashParams<T>): ArrayToHashResult<T> {
-  return {};
+  let hash = new Map<string, T>();
+
+  for (const item of array) {
+    if (!item[key]) continue;
+
+    hash.set(item[key] as string, item);
+  }
+
+  return Object.fromEntries(hash);
 }
 
 export { arrayToHash };
